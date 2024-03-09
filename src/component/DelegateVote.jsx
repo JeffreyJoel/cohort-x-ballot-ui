3e41c592496e5361d57598141bcc0f036822371f
JeffreyJoel <jowoloko@gmail.com>
2024-03-09 09:57:11 +0100

    add delegate and seperation of concerns

---
+++ src/component/DelegateVote.jsx
@@ -0,0 +1,75 @@
+import { Box, Button, Card, Flex, Text, TextField } from "@radix-ui/themes";
+import { useState } from "react";
+// import { isSupportedChain } from "../utils";
+// import { getProvider } from "../constants/providers";
+// import { getProposalsContract } from "../constants/contracts";
+// import { useWeb3ModalAccount, useWeb3ModalProvider } from "@web3modal/ethers/react";
+import useDelegate from "../hooks/useDelegate";
+
+const DelegateVote = () => {
+  const [address, setAddress] = useState("");
+
+//   const { chainId } = useWeb3ModalAccount();
+//   const { walletProvider } = useWeb3ModalProvider();
+
+
+    const handleDelegate = useDelegate(address);
+
+
+//   const handleDelegate = async (to) => {
+//     if (!isSupportedChain(chainId)) return console.error("Wrong network");
+//     const readWriteProvider = getProvider(walletProvider);
+//     const signer = await readWriteProvider.getSigner();
+
+//     const contract = getProposalsContract(signer);
+
+//     try {
+//         const transaction = await contract.delegate(to);
+//         console.log("transaction: ", transaction);
+//         const receipt = await transaction.wait();
+
+//         console.log("receipt: ", receipt);
+
+//         if (receipt.status) {
+//             return console.log("delegate successfull!");
+//         }
+
+//         console.log("delegate failed!");
+//     } catch (error) {
+//         console.log(error);
+//         // let errorText;
+//         // if (error.reason === "Self-delegation is disallowed") {
+//         //     errorText = "You cannot delegate yourself";
+//         // } else if (error.reason === "Already voted.") {
+//         //     errorText = "You have already voted";
+//         // } else {
+//         //     errorText = "An unknown error occured";
+//         // }
+// Found loop in delegation.
+//         // console.error("error: ", errorText);
+//     }
+// };
+  return (
+    <Card size="2" style={{ width: 425 }}>
+      <Flex gap="" align="center">
+        <Box width={"100%"}>
+          <Flex direction="column" gap="3">
+            <label>
+              <Text as="div" size="2" mb="1" weight="bold">
+                Delegate&apos;s Address
+              </Text>
+              <TextField.Input
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                placeholder="Enter Delegate's Address"
+              />
+            </label>
+            <Button onClick={handleDelegate}>Delegate vote</Button>
+          </Flex>
+        </Box>
+      </Flex>
+    </Card>
+  );
+};
+
+export default DelegateVote;
---
+++ src/hooks/useDelegate.js
@@ -0,0 +1,50 @@
+import {
+  useWeb3ModalAccount,
+  useWeb3ModalProvider,
+} from "@web3modal/ethers/react";
+import { getProposalsContract } from "../constants/contracts";
+import { getProvider } from "../constants/providers";
+import { isSupportedChain } from "../utils";
+
+const useDelegate = (to) => {
+  const { chainId } = useWeb3ModalAccount();
+  const { walletProvider } = useWeb3ModalProvider();
+
+  return async () => {
+    if (!isSupportedChain(chainId)) return console.error("Wrong network");
+    const readWriteProvider = getProvider(walletProvider);
+    const signer = await readWriteProvider.getSigner();
+
+    const contract = getProposalsContract(signer);
+
+    try {
+      const transaction = await contract.delegate(to);
+      console.log("transaction: ", transaction);
+      const receipt = await transaction.wait();
+
+      console.log("receipt: ", receipt);
+
+      if (receipt.status) {
+        return console.log("delegate successfull!");
+      }
+
+      console.log("delegate failed!");
+    } catch (error) {
+      console.log(error);
+
+      let errorText;
+      if (error.reason === "Self-delegation is disallowed") {
+        errorText = "You cannot delegate yourself";
+      } else if (error.reason === "Already voted.") {
+        errorText = "You have already voted";
+      } else if (error.reason === "Found loop in delegation.") {
+        errorText = "We Found loop in delegation.";
+      } else {
+        errorText = "An unknown error occured";
+      }
+      console.log(errorText);
+    }
+  };
+};
+
+export default useDelegate;
